676f72f491ce3a88f1301f1abd46e588acb7e4c4
Cemil Ozcan <71188464+cemil3535@users.noreply.github.com>
2024-10-10 00:53:27 +0300

    Update GameEntity.cs

--- CodeFirstBasic/Entities/GameEntity.cs
+++ CodeFirstBasic/Entities/GameEntity.cs
@@ -5,7 +5,7 @@
         
         public string Name { get; set; }
 
-        [RegularExpression("^(PC|PlayStation|Xbox)$", ErrorMessage = "Gecerli bir Platform degil")]
+        
         public string Platform {  get; set; }
 
         [Range(0, 10, ErrorMessage = "Oyun Puani 0 ile 10 arasinda olmalidir.")]
