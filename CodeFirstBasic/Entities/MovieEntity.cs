2855b08bdc68686ceb782f70d34bc21a5e4bea20
Cemil Ozcan <71188464+cemil3535@users.noreply.github.com>
2024-10-10 00:45:19 +0300

    Update MovieEntity.cs

--- CodeFirstBasic/Entities/MovieEntity.cs
+++ CodeFirstBasic/Entities/MovieEntity.cs
@@ -5,6 +5,7 @@
        
         public string Title { get; set; }
 
+        [RegularExpression("^(Action|Comedy|Drama)$", ErrorMessage = "Gecerli bir film turu degil")]
         public string Genre { get; set; }
 
         public int ReleaseYear { get; set; }
