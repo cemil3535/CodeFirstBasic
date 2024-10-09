5e61b0fd0c3c1ee07b1d3a76f101cfb527e44dcd
Cemil Ozcan <71188464+cemil3535@users.noreply.github.com>
2024-10-10 01:08:52 +0300

    Update PatikaFirstDbContext.cs

--- CodeFirstBasic/Context/PatikaFirstDbContext.cs
+++ CodeFirstBasic/Context/PatikaFirstDbContext.cs
@@ -15,13 +15,13 @@ namespace CodeFirstBasic.Context
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Game>(game =>
+            modelBuilder.Entity<GameEntity>(game =>
             {
                 game.ToTable("Games");
                 game.HasKey(x => x.Id);
                 game.Property(x => x.Rating).HasColumnType("decimal(3,2)");
             });
-            modelBuilder.Entity<Movie>(movie =>
+            modelBuilder.Entity<MovieEntity>(movie =>
             {
                 movie.ToTable("Movies");
                 movie.HasKey(x => x.Id);
